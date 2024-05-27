a71f2e47b45a29bba379b37de8ead488a7d79a3e
fabian <gf_gamboa@gmail.com>
2024-05-27 15:49:35 -0500

    2_Segundo: Hola Mundo

--- inventario/productos/views.py
+++ inventario/productos/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+def holaMundo(request):
+    return HttpResponse("Hola Mundo ADSO");
 
 # Create your views here.
