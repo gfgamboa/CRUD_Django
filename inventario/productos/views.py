c2ff7c99592f357d9828bd33a774f5e0389ae672
fabian <gf_gamboa@gmail.com>
2024-06-06 13:02:08 -0500

    3_Tercero: Página de inicio

--- inventario/productos/views.py
+++ inventario/productos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
+
 def holaMundo(request):
-    return HttpResponse("Hola Mundo ADSO");
+    return HttpResponse("Hola Mundo ADSO")
+
+def inicio(request):
+    return render(request, 'inicio.html')
 
 # Create your views here.
